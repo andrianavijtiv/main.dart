635e3131604a55a24489b77ceda636a59c09f0d6
andrianavijtiv <andrianavijtiv@gmail.com>
2024-10-16 07:11:53 +0300

    Create main.dart
    https://github.com/andrianavijtiv/main.dart.git

---
+++ main.dart
@@ -0,0 +1,23 @@
+void main() {
+ bool isSimple(int value) {
+
+    if (value %== 1) {
+
+        return false;
+
+    }
+
+{
+
+        if (value %  == 0) {
+
+            return false;
+
+        }
+
+    }
+
+    return true;
+    
+  }
+}
